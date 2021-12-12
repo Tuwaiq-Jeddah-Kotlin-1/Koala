c6745ca90a39908c965bb51a0f3cf411bebf71b4
Basil19 <bojsl-9@hotmail.com>
2021-12-12 17:04:02 +0300

    Delete Article -> Done

--- app/src/main/java/com/albasil/finalprojectkotlinbootcamp/Adapter/ArticleUserProfileAdapter.kt
+++ app/src/main/java/com/albasil/finalprojectkotlinbootcamp/Adapter/ArticleUserProfileAdapter.kt
@@ -1,10 +1,9 @@
 package com.albasil.finalprojectkotlinbootcamp.Adapter
 
-import android.graphics.BitmapFactory
-import android.graphics.Color
+import android.app.AlertDialog
+import android.content.Context
 import android.transition.AutoTransition
 import android.transition.TransitionManager
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,18 +11,16 @@ import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
-import androidx.core.view.isGone
-import androidx.core.view.isVisible
 import androidx.fragment.app.findFragment
-import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.NavHostFragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
-import coil.load
 import com.albasil.finalprojectkotlinbootcamp.R
-import com.albasil.finalprojectkotlinbootcamp.UI.HomePageDirections
 import com.albasil.finalprojectkotlinbootcamp.data.Article
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.storage.FirebaseStorage
-import java.io.File
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.up_date_user_information.view.*
 
 //class ArticleUserProfileAdapter(private val articleList:ArrayList<Article>) RecyclerView.Adapter<ArticleAdapter.UserViewHolder>() {
 
@@ -54,7 +51,6 @@ import java.io.File
 
 
 
-
         if (currentUserUid == holder.userID.toString()) {
 
 
@@ -62,25 +58,6 @@ import java.io.File
                 holder.editLinear.visibility = View.VISIBLE
 
         }
-      /*  if (currentUserUid == holder.userID.toString()){
-
-           // holder.editLinear.isVisible
-            //holder.articleCategory.setTextColor(Color.GREEN)
-
-            holder.editLinear.isVisible
-
-            if (holder.editLinear.visibility == View.GONE){
-                holder.editLinear.visibility == View.VISIBLE
-
-            }else{
-                holder.editLinear.visibility == View.GONE
-
-            }
-
-        }else{
-            holder.editLinear.isGone
-
-        }*/
 
 
 
@@ -105,7 +82,14 @@ import java.io.File
         val articleCategory : TextView =itemView.findViewById(R.id.articleCategory)
 
         val editLinear :LinearLayout =itemView.findViewById(R.id.editLinear)
+
+        val editArticle :ImageView = itemView.findViewById(R.id.imEditArticle_xml)
+        val deleteArticle :ImageView = itemView.findViewById(R.id.imDeleteArticle_xml)
+
        lateinit var userID :String
+       lateinit var articleID:String
+
+
 
 
 
@@ -116,34 +100,53 @@ import java.io.File
 
 
 
+        private fun deleteArticle() {
+            AlertDialog.Builder(itemView.context)
+                .setTitle("Delete Aricle")
+                .setIcon(R.drawable.common_google_signin_btn_icon_dark)
+                .setMessage("Are sure to delete this article ?!!!")
+                .setPositiveButton("yes") { dialog, _ ->
+
+
+                    //-------------------------------
+
+                    /*** delete fun */
+
+
+                    val deleteArticle=Firebase.firestore.collection("Articles")
+                        .document("1DVexs5RhquJcVi4WXNU").delete()
+
+
+                    deleteArticle
+
+
+                    //----------------------------
+
+
+                    dialog.dismiss()
+                }
+                .setNegativeButton("No") { dialog, _ ->
+                    dialog.dismiss()
+                }.create().show()
+        }
+
+
+
         override fun onClick(v: View?) {
 
-            date.setOnClickListener {
-                if (editLinear.visibility == View.GONE){
-                    editLinear.visibility == View.VISIBLE
+            deleteArticle.setOnClickListener {
+                deleteArticle()
+                Toast.makeText(itemView.context,"Delete Article",Toast.LENGTH_SHORT).show()
+            }
 
-                }else{
-                    editLinear.visibility == View.GONE
 
-                }
+            editArticle.setOnClickListener {
+                findNavController(itemView.findFragment()).navigate(R.id.apiFragment)
+
             }
 
 
             Toast.makeText(itemView.context,"${titleArticle.text.toString()} , ${articleCategory.text.toString()}",Toast.LENGTH_SHORT).show()
-          /*  val article_data = Article()
-
-            article_data.title = titleArticle.text.toString()
-            article_data.userName = userName.text.toString()
-            article_data.date = date.text.toString()
-            article_data.category = articleCategory.toString()
-            article_data.description = articleDescription.toString()
-            article_data.articleImage = image.toString()
-
-
-            val itemData = HomePageDirections.actionHomePageToArticleInformation(article_data)
-            NavHostFragment.findNavController(itemView.findFragment()).navigate(itemData)
-//          findNavController(itemView.findFragment()).navigate(R.id.action_homePage_to_articleInformation)
-*/
 
 
         }
